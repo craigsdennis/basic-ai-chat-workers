3f8d0ca85c5cf0d3a265eb4b79d3aa6a35631a3a
Craig Dennis <craigsdennis@gmail.com>
2025-05-09 14:34:36 -0700

    Added Workers AI chat endpoint

--- worker/index.ts
+++ worker/index.ts
@@ -1,9 +1,33 @@
-import { Hono } from 'hono';
+import { Hono } from "hono";
+import { events } from 'fetch-event-stream';
+import { streamText } from "hono/streaming";
+
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/api/', async(c) => {
-    return c.json({name: "Hono"});
-});
+app.post("/api/chat", async (c) => {
+  const payload = await c.req.json();
+  const messages = [...payload.messages];
+  // Prepend the systemMessage
+  if (payload?.config?.systemMessage) {
+    messages.unshift({ role: "system", content: payload.config.systemMessage });
+  }
+  const eventSourceStream = await c.env.AI.run("@cf/meta/llama-4-scout-17b-16e-instruct", {
+    messages,
+    stream: true
+  });
+	c.header('Content-Encoding', 'Identity');
+	return streamText(c, async (stream) => {
+		const chunks = events(new Response(eventSourceStream));
+		for await (const chunk of chunks) {
+			if (chunk.data !== undefined && chunk.data !== '[DONE]') {
+				const data = JSON.parse(chunk.data);
+				const token = data.response;
+				if (token) {
+					stream.write(token);
+				}
+			}
+		}
+	});});
 
 export default app;
